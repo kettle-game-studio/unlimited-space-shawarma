3fe550966a6ea4b66d281cc6e9255587381523c8
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2023-09-30 16:15:10 +0200

    Add door open script

--- assets/scripts/player_controller.gd
+++ assets/scripts/player_controller.gd
@@ -5,7 +5,6 @@ extends Node3D
 
 @export var camera_speed: float = 0.01
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
