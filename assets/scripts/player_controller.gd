1e94203a0ac2ab49aac9f31f54b8545630b7973f
Tetrergeru <41305740+Tetrergeru@users.noreply.github.com>
2023-09-30 14:59:44 +0200

    Add simple player controller

---
+++ assets/scripts/player_controller.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@export var player: Node3D
+@export var camera: Camera3D
+
+@export var camera_speed: float = 0.01
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		rotate_camera(event.relative)	
+
+func rotate_camera(vector: Vector2):
+	camera.rotation.x = clamp(camera.rotation.x - vector.y * camera_speed, -PI/2, PI/2)
+	player.rotate_y(-vector.x * camera_speed)
