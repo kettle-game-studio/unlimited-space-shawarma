33d1c81cad6522cdc80fd913b4591ff2d2d68e70
RobolabGs2 <slavikelk@yandex.ru>
2023-10-01 22:51:22 +0300

    Add door and computer sounds

--- assets/scripts/door_script.gd
+++ assets/scripts/door_script.gd
@@ -9,6 +9,9 @@ signal is_closed
 @export var targetPosition: Vector3
 @export var seconsToOpen: float
 
+@export var movingSound: AudioStreamPlayer3D
+@export var stopSound: AudioStreamPlayer3D
+
 var leftDoorStartPosition: Vector3
 var rightDoorStartPosition: Vector3 
 
@@ -20,23 +23,33 @@ func _ready():
 	rightDoorStartPosition = rightDoor.transform.origin
 
 func open():
-	const delta_time = 1.0 / 60.0 
+	const delta_time = 1.0 / 60.0
+	var stop_sound_lenght = stopSound.stream.get_length()
 	var time = 0;
+	movingSound.play()
 	while time < seconsToOpen:
 		time += delta_time
 		await get_tree().create_timer(delta_time).timeout
 		moved = time / seconsToOpen
 		set_doors_positions()
+		if !stopSound.playing && (seconsToOpen - time) <= stop_sound_lenght:
+			stopSound.play()
 	is_open.emit()
+	movingSound.stop()
 	
 func close():
 	const delta_time = 1.0 / 60.0 
+	var stop_sound_lenght = stopSound.stream.get_length()
 	var time = 0;
+	movingSound.play()
 	while time < seconsToOpen:
 		time += delta_time
 		await get_tree().create_timer(delta_time).timeout
 		moved = 1 - time / seconsToOpen
 		set_doors_positions()
+		if !stopSound.playing && (seconsToOpen - time) <= stop_sound_lenght:
+			stopSound.play()
+	movingSound.stop()
 	is_closed.emit()
 
 func set_doors_positions():
